f6387bfe1574a26369e8425d4308226a66c3a46c
memeyers68 <130182792+memeyers68@users.noreply.github.com>
2023-10-03 16:34:10 -0400

    Update main.tf added provider section

--- aria-automation/main.tf
+++ aria-automation/main.tf
@@ -6,3 +6,9 @@ terraform {
   }
   required_version = ">= 0.12"
 }
+
+provider "vra" {
+  url           = "https://www.mgmt.cloud.vmware.com"
+  refresh_token = "AKGdvexFqfFZBWSfxzBkJdu13pCunWv4n0_uPBFg1bBMcKDrKJyU_wDGTXFEAKtP"
+  insecure      = false
+}
