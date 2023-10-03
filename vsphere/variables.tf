765525fc8bcc22f632877384f68dc2ff6a02023c
memeyers68 <130182792+memeyers68@users.noreply.github.com>
2023-10-03 16:20:14 -0400

    Update variables.tf
    Removed the user names and passwords

--- vsphere/variables.tf
+++ vsphere/variables.tf
@@ -1,11 +1,3 @@
-variable "username" {
-  default = "mike.meyers@vsphere.local"
-}
-
-variable "password" {
-  default = "9j2YUCJ5@CJeCU@nho4Vr!"
-}
-
 variable "vcenter" {
   default = "10.100.3.16"
 }
@@ -21,12 +13,4 @@ variable "compute" {
 variable "storage" {
   #default = "TRO-STOR-CLU"
   default = "V7K-2-DS17"
-}
-
-variable "domain_user" {
-  default = "tyler\\elmmeyers"
-}
-
-variable "domain_password" {
-  default = "S@X0n68.S#!T!"
 }
