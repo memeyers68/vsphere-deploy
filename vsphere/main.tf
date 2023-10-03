77207df21c91b644335f78d57ceb66d538e8cb38
memeyers68 <130182792+memeyers68@users.noreply.github.com>
2023-10-03 17:32:52 -0400

    Update main.tf - removed a commented line

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "vsphere" {
   /* user           = var.username
-  password       = var.password
+  password       = var.password*/
   vsphere_server = var.vcenter
   
-  # If you have a self-signed cert*/
+  # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
